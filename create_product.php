d672313a4f44be57842b566b7dc37fed800e4d11
ben_ska <70025558+Niko-ui77@users.noreply.github.com>
2022-06-13 16:27:36 +0500

    add

---
+++ create_product.php
@@ -0,0 +1,12 @@
+<?php
+// установка заголовка страницы
+$page_title = "Создание товара";
+
+require_once "layout_header.php";
+?>
+
+<!-- здесь будет контент -->
+
+<?php // подвал
+require_once "layout_footer.php";
+?>

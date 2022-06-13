4ea700c7cc93487643ff814d030726e80ea17861
ben_ska <70025558+Niko-ui77@users.noreply.github.com>
2022-06-13 19:46:57 +0500

    add

--- create_product.php
+++ create_product.php
@@ -5,7 +5,11 @@ $page_title = "Создание товара";
 require_once "layout_header.php";
 ?>
 
-<!-- здесь будет контент -->
+<div class="right-button-margin">
+    <a href="index.php" class="btn btn-default pull-right">Просмотр всех товаров</a>
+</div>
+
+<!-- здесь будет html-форма "create product" -->
 
 <?php // подвал
 require_once "layout_footer.php";

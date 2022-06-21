397829fcb52fedae22e211b93d6e46728f53c8e3
ben_ska <70025558+Niko-ui77@users.noreply.github.com>
2022-06-21 15:53:19 +0500

    POM
    Creat

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+// установка заголовка страницы
+$page_title = "Вывод товаров";
+
+require_once "layout_header.php";
+?>
+
+<div class="right-button-margin">
+    <a href="create_product.php" class="btn btn-default pull-right">Добавить товар</a>
+</div>
+
+<?php // подвал
+require_once "layout_footer.php";
+?>

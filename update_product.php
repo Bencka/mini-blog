dc93700fe8c4f72863291afa63510f2dab58ca25
ben_ska <70025558+Niko-ui77@users.noreply.github.com>
2022-07-04 16:17:35 +0500

    POM
    add "update"

--- update_product.php
+++ update_product.php
@@ -31,7 +31,87 @@ include_once "layout_header.php";
     <a href="index.php" class="btn btn-default pull-right">Просмотр всех товаров</a>
 </div>
 
-<!-- здесь будет форма обновления товара -->
+<?php
+// если форма была отправлена (submit)
+if ($_POST) {
+
+    // устанавливаем значения свойствам товара
+    $product->name = $_POST["name"];
+    $product->price = $_POST["price"];
+    $product->description = $_POST["description"];
+    $product->category_id = $_POST["category_id"];
+
+    // обновление товара
+    if ($product->update()) {
+        echo "<div class='alert alert-success alert-dismissable'>";
+            echo "Товар был обновлён.";
+        echo "</div>";
+    }
+
+    // если не удается обновить товар, сообщим об этом пользователю
+    else {
+        echo "<div class='alert alert-danger alert-dismissable'>";
+            echo "Невозможно обновить товар.";
+        echo "</div>";
+    }
+}
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}");?>" method="post">
+    <table class="table table-hover table-responsive table-bordered">
+
+        <tr>
+            <td>Название</td>
+            <td><input type="text" name="name" value="<?php echo $product->name; ?>" class="form-control" /></td>
+        </tr>
+
+        <tr>
+            <td>Цена</td>
+            <td><input type="text" name="price" value="<?php echo $product->price; ?>" class="form-control" /></td>
+        </tr>
+
+        <tr>
+            <td>Описание</td>
+            <td><textarea name="description" class="form-control"><?php echo $product->description; ?></textarea></td>
+        </tr>
+
+        <tr>
+            <td>Категория</td>
+            <td>
+            <?php
+                $stmt = $category->read();
+
+                // помещаем категории в выпадающий список
+                echo "<select class='form-control' name='category_id'>";
+
+                    echo "<option>Пожалуйста выберите...</option>";
+                    while ($row_category = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        $category_id = $row_category["id"];
+                        $category_name = $row_category["name"];
+
+                        // необходимо выбрать текущую категорию товара
+                        if ($product->category_id == $category_id) {
+                            echo "<option value='$category_id' selected>";
+                        } else {
+                            echo "<option value='$category_id'>";
+                        }
+
+                        echo "$category_name</option>";
+                    }
+                echo "</select>";
+                ?>
+            </td>
+        </tr>
+
+        <tr>
+            <td></td>
+            <td>
+                <button type="submit" class="btn btn-primary">Обновить</button>
+            </td>
+        </tr>
+
+    </table>
+</form>
 
 <?php // подвал
 require_once "layout_footer.php";

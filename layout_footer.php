2b544a6bb4b88895d7269da7d9323462e379eb0a
ben_ska <70025558+Niko-ui77@users.noreply.github.com>
2022-07-06 16:24:26 +0500

    POD
    update

--- layout_footer.php
+++ layout_footer.php
@@ -9,5 +9,40 @@
 
 <!-- bootbox library -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/4.4.0/bootbox.min.js"></script>
+
+<script>
+    // JavaScript для удаления товара
+    $(document).on("click", ".delete-object", function() {
+        const id = $(this).attr("delete-id");
+    
+        bootbox.confirm({
+            message: "<h4>Вы уверены?</h4>",
+            buttons: {
+                confirm: {
+                    label: "<span class='glyphicon glyphicon-ok'></span> Да",
+                    className: "btn-danger"
+                },
+                cancel: {
+                    label: "<span class='glyphicon glyphicon-remove'></span> Нет",
+                    className: "btn-primary"
+                }
+            },
+            callback: function (result) {
+                if (result == true) {
+                    $.post("delete_product.php", {
+                        object_id: id
+                    }, function(data){
+                        location.reload();
+                    }).fail(function() {
+                        alert("Невозможно удалить.");
+                    });
+                }
+            }
+        });
+    
+        return false;
+    });
+</script>
+
 </body>
 </html>
--- objects/product.php
+++ objects/product.php
@@ -144,5 +144,21 @@ class Product {
     
         return false;
     }
+    
+    // удаление товара
+function delete() {
+
+    // запрос MySQL для удаления
+    $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(1, $this->id);
+
+    if ($result = $stmt->execute()) {
+        return true;
+    } else {
+        return false;
+    }
+}
 }
 ?>
